3e5126a87ccddb481e303c8dd7e83544b2ceab01
Dorian Beganovic <beganovic.dorian@gmail.com>
2021-03-07 08:30:44 +0000

    commit final results

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Author: Dorian Beganovic   
+
 ##### Academic declaration
 
 I have read and understood the sections on plagiarism in the College Policy on assessment offences and confirm that the work is my own, with the work of others clearly acknowledged. I give my permission to submit my work to the plagiarism testing database that the College is using and test it using plagiarism detection software, search engines or meta-searching software.
@@ -7,3 +9,7 @@ I have read and understood the sections on plagiarism in the College Policy on a
 To run the application, Python version 3.6 is required.  
 In Python, please install the dependencies from the requirements.txt file (eg. `pip install -r requirements.txt`).    
 Then you can run the program using:  `python cli.py` 
+
+## Results
+
+The answers to questions 1-5 are in the results.txt file.
--- cli.py
+++ cli.py
@@ -1,59 +1,96 @@
 from py2neo import Graph
 
-initial_prompt = """
+user = "neo4j"
+password = "pass123"
+## assumes neo4j running locally
+graph = Graph("bolt://localhost", auth=(user, password))
+
+prompts = {
+    "initial_prompt": """
 1) Find the party that won a given constituency i.e. received the highest number of votes.
-    -- required input: the constituency you are interested in
+    -- please input: the constituency you are interested in
 2) Find the list of parties that contested a given constituency, in order of decreasing number of votes, plus the votes that each received.
-    -- required input: the constituency you are interested in
+    -- please input: the constituency you are interested in
 3) Find the party that won the election i.e. won the most constituencies.
 4) Find the list of parties that contested the election, in order of decreasing number of constituencies won, plus the number of constituencies they each won.
 5) Find the constituencies where a given party lost its deposit i.e. won less than 5% of the votes cast at that constituency.
-    -- required input: name of the party
+    -- please input: name of the party
 6) Find all pairs (c,p) such that party p lost its deposit at constituency c.
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-"""
+""",
 
-constituency_prompt = """
+    "constituency_prompt": """
 Please provide the name of the constituency you are interested in
-"""
+""",
 
-party_prompt = """
+    "party_prompt": """
 Please provide the name of the party you are interested in
 """
+}
 
-user = "neo4j"
-password = "pass123"
-# driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth(user, password))
-graph = Graph("bolt://localhost", auth=(user, password))
+queries = {
+    1: """
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukc.ukarea = "{}"
+        WITH max(ukr.ukvotes) as max_votes
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukr.ukvotes = max_votes
+        RETURN ukp.party as winning_party_in_constituency    
+    """,
+    2: """
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukc.ukarea = "{}"
+        RETURN ukp.party as party, ukr.ukvotes as num_votes
+        ORDER BY ukr.ukvotes DESC
+    """,
+    3: """
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WITH ukc.ukarea as ukarea, max(ukr.ukvotes) as max_votes
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukc.ukarea = ukarea and ukr.ukvotes = max_votes
+        WITH ukp.party as party, count(*) as constituencies_won
+        RETURN party as winning_party
+        ORDER BY constituencies_won DESC
+        LIMIT 1
+    """,
+    5: """
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WITH ukc.ukarea as ukarea, sum(ukr.ukvotes) as total_votes
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukc.ukarea = ukarea and ukr.ukvotes < 0.05 * total_votes and ukp.party="{}"
+        RETURN ukc.ukarea as constituencies_where_party_lost_deposit
+        ORDER BY constituencies_where_party_lost_deposit
+    """,
+    6: """
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WITH ukc.ukarea as ukarea, sum(ukr.ukvotes) as total_votes
+        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
+        WHERE ukc.ukarea = ukarea and ukr.ukvotes < 0.05 * total_votes
+        RETURN ukp.party, ukc.ukarea as constituencies_where_party_lost_deposit
+        ORDER BY ukp.party, constituencies_where_party_lost_deposit
+    """
+}
+
+prompts_for_query = {
+    1: "constituency_prompt",
+    2: "constituency_prompt",
+    5: "party_prompt",
+}
 
 
-def run():
-    question = get_input_query()
-
-    if question == 1:
-        constituency = get_other_input(constituency_prompt)
-        query1(constituency)
-
-    elif question == 2:
-        constituency = get_other_input(constituency_prompt)
-        query2(constituency)
-
-    elif question == 3:
-        query3()
-
-    elif question == 4:
-        pass
+def execute_query(query):
+    prompt = prompts_for_query.get(query, None)
+    if prompt is None:
+        run_query(queries[query])
+    else:
+        run_query(queries[query].format(get_additional_input_from_user(prompts[prompts_for_query[query]])))
 
 
-    elif question == 5:
-        party = get_other_input(party_prompt)
+def run_query(query):
+    print(graph.run(query).to_table())
 
 
-    elif input == 6:
-        pass
-
-
-def get_other_input(prompt):
+def get_additional_input_from_user(prompt):
     datainput = None
     while datainput is None:
         datainput = validate_str_input(input(prompt))
@@ -61,71 +98,24 @@ def get_other_input(prompt):
     return datainput
 
 
-def get_input_query():
-    question = None
-    while not isinstance(question, int) or not question in [1, 2, 3, 4, 5, 6]:
+def get_query_user_wants():
+    query = None
+    while not isinstance(query, int) or not query in [1, 2, 3, 4, 5, 6]:
         try:
             print("Please provide an integer in range 1 to 6")
-            question = int(input(initial_prompt))
+            query = int(input(prompts["initial_prompt"]))
         except Exception as e:
             print("Please provide an integer in range 1 to 6")
-    return question
+    return query
 
 
 def validate_str_input(input):
     if not isinstance(input, str):
         print("Please enter a correct string")
         return None
+    input = input.lstrip(' ').rstrip(' ')
     return input
 
 
-def query1(constituency):
-    res = graph.run("""
-        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
-        WHERE ukc.ukarea = "{}"
-        WITH max(ukr.ukvotes) as max_votes
-        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
-        WHERE ukr.ukvotes = max_votes
-        RETURN ukp.party
-    """.format(constituency)).to_table()
-    print(res)
-
-
-def query2(constituency):
-    res = graph.run("""
-        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
-        WHERE ukc.ukarea = "{}"
-        RETURN ukp.party, ukr.ukvotes
-        ORDER BY ukr.ukvotes DESC
-    """.format(constituency)).to_table()
-    print(res)
-
-
-def query3():
-    res = graph.run("""
-        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
-        WITH ukc.ukarea as ukarea, max(ukr.ukvotes) as max_votes
-        MATCH  (ukc:Ukconst)-[ukr:Ukresult]->(ukp:Ukparty) 
-        WHERE ukc.ukarea = ukarea and ukr.ukvotes = max_votes
-        WITH ukp.party as party, count(*) as constituencies_won
-        RETURN party, constituencies_won
-        ORDER BY constituencies_won DESC
-        LIMIT 1
-    """).to_table()
-    print(res)
-
-
-def query4():
-    pass
-
-
-def query5(party):
-    pass
-
-
-def query6():
-    pass
-
-
 if __name__ == '__main__':
-    run()
+    execute_query(get_query_user_wants())
